9957a87ee58730efa0d946da23b7060a79a0ec30
lilly <ellis.l@rangiruru.school.nz>
2020-04-02 10:31:06 +1300

    added adding to the todo list

--- src/index.svelte
+++ src/index.svelte
@@ -1,13 +1,29 @@
 <script>
+let newTodo =""
 let todos = [
     "wash the dog",
     "vaccume the carpet"
 
 ]
+
+function addTodo(){
+    //add the newTodo to the end of the todos array
+    todos = [...todos, newTodo]
+}
+
 </script>
 
 
 <h1 class="title">To-do list</h1>
+
+<label>
+Tdo:
+<input type="text" bind:value={newTodo}> 
+</label>
+
+
+<button on:click={addTodo}>Add</button>
+
 
 <ul class="list">
 {#each todos as todo}
